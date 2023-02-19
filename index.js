ba5e9a2d1c29953489c121d09e803f56653e9bcc
Paju980 <prajwalwandhare100@gmail.com>
2023-02-19 14:46:05 +0530

    "first commit"

---
+++ index.js
@@ -0,0 +1,18 @@
+let string = "";
+let buttons = document.querySelectorAll('button');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+        }else if(e.target.innerHTML == 'AC'){
+            string = ""
+        }else if(e.target.innerHTML == 'D'){
+            string = string.substring(0,string.length-1);
+        }else{
+            string = string + e.target.innerHTML;
+        }
+        document.querySelector('input').value = string;
+    })
+})
